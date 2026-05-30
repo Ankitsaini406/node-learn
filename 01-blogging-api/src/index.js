bc44b627a9ee40af4db0a21665b74c86a746e5fe
Ankit Saini <as.ankitsaini406@gmail.com>
2026-05-30 11:57:23 +0530

    add new files

---
+++ 01-blogging-api/src/index.js
@@ -0,0 +1,25 @@
+import app from "./app.js";
+
+const PORT = process.env.PORT || 5000;
+
+async function startServer() {
+    try {
+        const server = app.listen(PORT, () => {
+            console.log(`🚀 Server running on port ${PORT}`);
+        });
+        process.on("SIGTERM", () => shutdown(server));
+        process.on("SIGINT", () => shutdown(server));
+
+    } catch (error) {
+        console.error("❌ Failed to connect to database:", error.message);
+        process.exit(1);
+    }
+};
+
+function shutdown(server) {
+    console.log("🛑 Shutting down server...");
+    server.close(() => {
+        console.log("💤 Server closed");
+        process.exit(0);
+    });
+}
