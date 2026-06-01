a7767552d70d3e837d7796e5515f0f9f4f545db4
Ankit Saini <as.ankitsaini406@gmail.com>
2026-06-01 10:18:56 +0530

    frontend file location changed and add user model

---
+++ 02-todolist-api/src/models/user.models.js
@@ -0,0 +1,61 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+        },
+        email: {
+            type: String,
+            unique: true,
+            index: true,
+            trim: true,
+            required: [true, 'Email is required']
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        token: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+/* ------------------ HASH PASSWORD ------------------ */
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+/* ------------------ COMPARE PASSWORD ------------------ */
+
+userSchema.method.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+/* ------------------ GENERATE ACCESS TOKEN ------------------ */
+
+userSchema.method.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY || "1d",
+        }
+    );
+};
+
+export const user = mongoose.model("User", userSchema);
