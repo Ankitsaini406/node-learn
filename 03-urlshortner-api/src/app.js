028ac573bb3b41733efdb29999b80d1fdd5fc240
Ankit Saini <as.ankitsaini406@gmail.com>
2026-06-26 12:56:10 +0530

    update db and app file add express and mongoose

---
+++ 03-urlshortner-api/src/app.js
@@ -0,0 +1,24 @@
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: "16kb",
+    })
+);
+
+const api = "/api";
+
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: `Route ${req.originalUrl} not found`,
+        method: req.method,
+    });
+});
+
+export default app;
